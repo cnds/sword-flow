b04a1507f82652ef5a15bf15b2d294c5ee328b64
yuchenyang1994 <yuchenyang1994@foxmail.com>
2020-03-10 00:15:32 +0800

    :sparkles:  change the models

--- models.go
+++ models.go
@@ -13,7 +13,6 @@ type SfWorkFlows struct {
 	description string          `gorm:"type:text"`
 	IsValid     bool            `gorm:"default:false"`
 	IsDraft     bool            `gorm:"default:true"`
-	CreatorID   uint            `gorm:"type:bigint"`
 	ErrorMsg    string          `gorm:"type:text"`
 	Places      []SfPlaces      `gorm:"foreignkey:WorkflowID"`
 	Transitions []SfTransitions `gorm:"foreignkey:WorkflowID"`
@@ -41,55 +40,38 @@ type SfPlaces struct {
 // Each record will point to an application task within the MENU database.
 type SfTransitions struct {
 	gorm.Model
-	WorkflowID  uint                      `gorm:"type:bigint"`
-	Name        string                    `gorm:"type:varchar(100)"`
-	Description string                    `gorm:"type:text"`
-	SortOrder   uint                      `gorm:"default:0"`
-	WorkFlow    SfWorkFlows               `gorm:"foreignkey:WorkflowID"`
-	Arcs        []SfArcs                  `gorm:"foreignkey:TransitionID"`
-	Assignments []SfTransitionAssignments `gorm:"foreignkey:TransitionID"`
-	TiggerLimit uint                      `gorm:"type:int"`
-	TiggerType  uint                      `gorm:"default:0"`
+	WorkflowID  uint        `gorm:"type:bigint"`
+	Name        string      `gorm:"type:varchar(100)"`
+	Description string      `gorm:"type:text"`
+	SortOrder   uint        `gorm:"default:0"`
+	WorkFlow    SfWorkFlows `gorm:"foreignkey:WorkflowID"`
+	Arcs        []SfArcs    `gorm:"foreignkey:TransitionID"`
+	TiggerLimit uint        `gorm:"type:int"`
+	TiggerType  uint        `gorm:"default:0"`
 }
 
 // SfArcs This table holds the details for each arc within a workflow process.
 // An arc links a place to a transition.
 type SfArcs struct {
 	gorm.Model
-	WorkflowID         uint   `gorm:"type:bigint"`
-	TransitionID       uint   `gorm:"type:bigint"`
-	PlaceID            uint   `gorm:"type:bigint"`
-	Direction          uint   `gorm:"default:0"`
-	ArcType            uint   `gorm:"default:0"`
-	ConditionField     string `gorm:"type:varchar(50)"`
-	ConditionOp        string `gorm:"type:varchar(50)"`
-	ConditionValue     string `gorm:"type:varchar(50)"`
-	ConditionExp       string `gorm:"type:varchar(50)"`
-	ConditionFieldType string `gorm:"type:varchar(50)"`
-}
-
-// SfTransitionAssignments This Tbale holds assign for RBAC
-type SfTransitionAssignments struct {
-	gorm.Model
-	WorkflowID     uint          `gorm:"type:bigint"`
-	TransitionID   uint          `gorm:"type:bigint"`
-	AssignableType string        `gorm:"type:varchar(100)"`
-	AssignableID   string        `gorm:"type:varchar(100)"`
-	Workflow       SfWorkFlows   `gorm:"foreignkey:WorkflowID"`
-	Transition     SfTransitions `gorm:"foreignkey:TransitionID"`
+	WorkflowID   uint   `gorm:"type:bigint"`
+	TransitionID uint   `gorm:"type:bigint"`
+	PlaceID      uint   `gorm:"type:bigint"`
+	Direction    uint   `gorm:"default:0"`
+	ArcType      string `gorm:"type:varchar(50)"`
 }
 
 // SfCases This identifies when a particular instance of a workflow was started,
 // its context and its current status.
 type SfCases struct {
 	gorm.Model
-	WorkflowID     uint          `gorm:"type:bigint"`
-	TargetableType uint          `gorm:"type:varchar(100)"`
-	TargetableID   uint          `gorm:"type:varchar(100)"`
-	State          uint          `gorm:"type:varchar(100)"`
-	Workflow       SfWorkFlows   `gorm:"foreignkey:WorkflowID"`
-	WorkItems      []SfWorkItems `gorm:"foreignkey:CaseID"`
-	Tokens         []SfTokens    `gorm:"foreignkey:CaseID"`
+	WorkflowID uint          `gorm:"type:bigint"`
+	State      string        `gorm:"type:varchar(100)"`
+	Workflow   SfWorkFlows   `gorm:"foreignkey:WorkflowID"`
+	WorkItems  []SfWorkItems `gorm:"foreignkey:CaseID"`
+	Tokens     []SfTokens    `gorm:"foreignkey:CaseID"`
+	StartAt    *time.Time
+	EndAt      *time.Time
 }
 
 // SfTokens This identifies when a token was inserted into a particular place.
@@ -97,10 +79,8 @@ type SfTokens struct {
 	gorm.Model
 	WorkflowID       uint        `gorm:"type:bigint"`
 	CaseID           uint        `gorm:"type:bigint"`
-	TargetableType   string      `gorm:"type:varchar(100)"`
-	TargetableID     string      `gorm:"type:varchar(100)"`
 	PlaceID          uint        `gorm:"type:bigint"`
-	State            uint        `gorm:"default:0"`
+	State            uint        `gorm:"type:varchar(50)"`
 	WorkItemID       uint        `gorm:"type:bigint"`
 	LockedWorkItemID uint        `gorm:"type:bigint"`
 	WorkFlow         SfWorkFlows `gorm:"foreignkey:WorkflowID"`
@@ -112,6 +92,23 @@ type SfTokens struct {
 	ConsumedAt       *time.Time
 }
 
+// AfterCreate on token create
+// Get a list of all places which input to the transition.
+// Check that each of these places has a token (an AND-join must have one token for each input place). If the correct number of tokens is found then
+// 1. enable the transition (create a new WORKITEM record) AND fire thre translation tigger
+// 2. If the transition trigger is 'AUTO' then process that TASK immediately, otherwise wait for some other trigger.
+func (token *SfTokens) AfterCreate(scope *gorm.Scope) (err error) {
+	scope.DB().Model(&token).Related(&token.Case)
+	scope.DB().Model(&token).Related(&token.Place)
+	if token.Place.PlaceType == 9 {
+		token.Case.State = "CLOSED"
+		scope.DB().Save(&token.Case)
+		return
+	}
+	// TODO Find all inward arcs which go from this place
+	return
+}
+
 // SfWorkItems A record is created here when a transition is enabled or able to fire.
 // Entries which are to be triggered by a human participant will appear on the Menu/Home Page of relevant users
 // so that they can see what tasks are pending and select any for processing.
@@ -119,21 +116,17 @@ type SfTokens struct {
 // will cause the relevant application task to be activated with the correct context already loaded.
 type SfWorkItems struct {
 	gorm.Model
-	WorkflowID     uint          `gorm:"type:bigint"`
-	CaseID         uint          `gorm:"type:bigint"`
-	TransitionID   uint          `gorm:"type:bigint"`
-	TargetableType string        `gorm:"type:varchar(100)"`
-	TargetableID   string        `gorm:"type:varchar(100)"`
-	State          uint          `gorm:"default:0"`
-	UserType       string        `gorm:"type:varchar(100)"`
-	UserID         uint          `gorm:"type:bigint"`
-	WorkFlow       SfWorkFlows   `gorm:"foreignkey:WorkflowID"`
-	Transition     SfTransitions `gorm:"foreignkey:TransitionID"`
-	Case           SfCases       `gorm:"foreignkey:CaseID"`
-	EnabledAt      *time.Time    `gorm:"default:CURRENT_TIMESTAMP"`
-	StartedAt      *time.Time
-	CanceledAt     *time.Time
-	FinishedAt     *time.Time
-	OverriddenAt   *time.Time
-	Deadline       *time.Time
+	WorkflowID   uint          `gorm:"type:bigint"`
+	CaseID       uint          `gorm:"type:bigint"`
+	TransitionID uint          `gorm:"type:bigint"`
+	State        uint          `gorm:"type:varchar(50)"`
+	WorkFlow     SfWorkFlows   `gorm:"foreignkey:WorkflowID"`
+	Transition   SfTransitions `gorm:"foreignkey:TransitionID"`
+	Case         SfCases       `gorm:"foreignkey:CaseID"`
+	EnabledAt    *time.Time    `gorm:"default:CURRENT_TIMESTAMP"`
+	StartedAt    *time.Time
+	CanceledAt   *time.Time
+	FinishedAt   *time.Time
+	OverriddenAt *time.Time
+	Deadline     *time.Time
 }
